d6312b1ba07f44f829519ecbea36989adbbf1db4
Lorenzo Xiao <53085041+Algoroxyolo@users.noreply.github.com>
2022-11-08 09:20:26 +0300

    Delete test.py

--- test.py
+++
@@ -1,8 +0,0 @@
-import os
-
-list_directory = os.listdir('.')
-filelists = []
-for directory in list_directory:
-    # os.path 模块稍后会讲到
-    if(os.path.isfile(directory)):
-        filelists.append(directory)
